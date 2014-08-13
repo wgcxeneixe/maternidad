228867e3085bacbebd088e76ec2a226cfc9a2cbf
strendek <strendek@gmail.com>
2014-08-12 21:11:41 -0300

    Algunos insert

---
+++ grails-app/conf/sql/SQL.sql
@@ -0,0 +1,12 @@
+INSERT INTO sami.pais VALUES (1,0,'AR','ARG.',true,'Argentinos','Argentina');
+
+INSERT INTO sami.provincia VALUES (1,0,'ER','Entre Rios',1);
+
+INSERT INTO sami.localidad VALUES (1,0,'CDELU',true,'3260','Conccepcion del Uruguay',1);
+
+
+INSERT INTO sami.tipo_documento VALUES (1,0,'DNI',true,'DNI','Doc. Nac. Identidad','Registro Civil','Doc. Nac. Identidad',1,1)
+
+INSERT INTO sami.estado_civil VALUES (1,0,'Solterazo');
+
+INSERT INTO sami.persona VALUES (1,0,'Migueles','Piedras 13','3260',20666666666,'Villaguay',1,1,1,'Matute',66666666,13,true,0,'SINRAZON',1);
