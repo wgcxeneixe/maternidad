5457d1c74fb3bb6c76bc241373758d683ccbf2b6
strendek <strendek@gmail.com>
2014-09-18 17:17:13 -0300

    Cuenta corriente profesional

---
+++ grails-app/controllers/maternidad/MovimientoProfesionalController.groovy
@@ -0,0 +1,105 @@
+package maternidad
+
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class MovimientoProfesionalController {
+
+    static scaffold = true
+
+    //static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond MovimientoProfesional.list(params), model: [movimientoProfesionalInstanceCount: MovimientoProfesional.count()]
+    }
+
+    def show(MovimientoProfesional movimientoProfesionalInstance) {
+        respond movimientoProfesionalInstance
+    }
+
+    def create() {
+        respond new MovimientoProfesional(params)
+    }
+
+    @Transactional
+    def save(MovimientoProfesional movimientoProfesionalInstance) {
+        if (movimientoProfesionalInstance == null) {
+            notFound()
+            return
+        }
+
+        if (movimientoProfesionalInstance.hasErrors()) {
+            respond movimientoProfesionalInstance.errors, view: 'create'
+            return
+        }
+
+        movimientoProfesionalInstance.save flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'movimientoProfesional.label', default: 'MovimientoProfesional'), movimientoProfesionalInstance.id])
+                redirect movimientoProfesionalInstance
+            }
+            '*' { respond movimientoProfesionalInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(MovimientoProfesional movimientoProfesionalInstance) {
+        respond movimientoProfesionalInstance
+    }
+
+    @Transactional
+    def update(MovimientoProfesional movimientoProfesionalInstance) {
+        if (movimientoProfesionalInstance == null) {
+            notFound()
+            return
+        }
+
+        if (movimientoProfesionalInstance.hasErrors()) {
+            respond movimientoProfesionalInstance.errors, view: 'edit'
+            return
+        }
+
+        movimientoProfesionalInstance.save flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'MovimientoProfesional.label', default: 'MovimientoProfesional'), movimientoProfesionalInstance.id])
+                redirect movimientoProfesionalInstance
+            }
+            '*' { respond movimientoProfesionalInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(MovimientoProfesional movimientoProfesionalInstance) {
+
+        if (movimientoProfesionalInstance == null) {
+            notFound()
+            return
+        }
+
+        movimientoProfesionalInstance.delete flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'MovimientoProfesional.label', default: 'MovimientoProfesional'), movimientoProfesionalInstance.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'movimientoProfesional.label', default: 'MovimientoProfesional'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NOT_FOUND }
+        }
+    }
+}
--- grails-app/domain/maternidad/MovimientoProfesional.groovy
+++ grails-app/domain/maternidad/MovimientoProfesional.groovy
@@ -2,25 +2,26 @@ package maternidad
 
 class MovimientoProfesional {
 
-    Double monto
     Date fecha
-    String observacion
-    Boolean credito
     Profesional profesional
     ConceptoProfesional conceptoProfesional
+    Boolean credito
+    Double monto
+    String observacion
     Usuario usuario
 
 
     static constraints = {
-
-    monto(nullable: false,blank:false)
-    observacion(size:0..5000, nullable:true, blank:true)
-    fecha(nullable: false,blank:false)
-    credito(nullable: false,blank:false)
-    profesional(nullable: true,blank:true)
-    conceptoProfesional(nullable: true,blank:true)
-    usuario(nullable: false,blank:false)
+        fecha(nullable: false,blank:false,format:'dd-MM-yyyy',validator: { val, obj ->val?.before(new Date())})
+        profesional(nullable: false,blank:false)
+        conceptoProfesional(nullable: true,blank:true)
+        credito(nullable: false,blank:false)
+        monto(nullable: false,blank:false, scale: 2, matches: "[0-9]")
+        observacion(size:0..5000, nullable:true, blank:true, widget:"textarea")
+        usuario(nullable: false,blank:false)
     }
+
+    String toString() { "Nº ${id}" }
 }
 
 
