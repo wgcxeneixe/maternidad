2b9dd4ddaf9400953bc5c4a913d012e1df7a36cf
lalvarez@cent.gov.ar <lalvarez@cent.gov.ar>
2014-09-29 19:43:31 -0300

    se cambia el DataSource para que no haga create al correr el proyecto sino que haga un update. Se le agrega la validación de que el usuario tenga el rol de admin para los scaffold de Factura, RetencionPago y TipoRetencionPago

--- grails-app/controllers/maternidad/TipoRetencionPagoController.groovy
+++ grails-app/controllers/maternidad/TipoRetencionPagoController.groovy
@@ -1,5 +1,8 @@
 package maternidad
 
+import grails.plugin.springsecurity.annotation.Secured
+
+@Secured("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
 class TipoRetencionPagoController {
 
     static scaffold = true
