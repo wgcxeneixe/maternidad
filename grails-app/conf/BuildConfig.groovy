c7dd11c1eb8102142e92b3d6e405a3a6c8eefb59
Walter Corazzini <waltercorazzini@gmail.com>
2015-01-21 13:04:44 -0300

    agrego el plugin remote pagination en build config

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -75,6 +75,7 @@ grails.project.dependency.resolution = {
         //compile ":less-asset-pipeline:1.10.0"
         //compile ":coffee-asset-pipeline:1.8.0"
         //compile ":handlebars-asset-pipeline:1.3.0.3"
+        compile ":remote-pagination:0.4.8"
     }
 
     //grails.server.port.http=8181
